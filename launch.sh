7edf7333587a72330d149d96881052cd39d9fd39
u18001808 <u18001808@tuks.co.za>
2024-11-07 09:11:23 +0200

    1.11.8: ls to see what is going on

--- launch.sh
+++ launch.sh
@@ -39,6 +39,7 @@ if ! [[ -f "OSR Server - ${FilesVer}" ]]; then
   rm -rf todelete
   curl -L "https://drive.usercontent.google.com/download?id=1g2EoeOEMTbhIN-CAuiIu5mS7Tzke0Hkq&confirm=xxx" -o ServerFiles.zip
   unzip ServerFiles.zip
+  ls
   mv OSR\ ${FilesVer}\ -\ ${tempVar}/* ./
   rm ServerFiles.zip
 fi
