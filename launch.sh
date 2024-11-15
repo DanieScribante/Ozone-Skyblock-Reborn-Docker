812c6be942d593de370c3737fa14422843e93b87
u18001808 <u18001808@tuks.co.za>
2024-11-15 13:21:25 +0200

    Folder Detection Fix

--- launch.sh
+++ launch.sh
@@ -32,7 +32,7 @@ fi
 #   rm -f ./mods/entity_model_features_forge_*.jar ./mods/entity_texture_features_forge_*.jar
 # fi
 
-if ! [[ -f "OSR Server - ${FilesVer}" ]]; then
+if ! [[ -f OSR\ Server\ -\ ${FilesVer} ]]; then
   mkdir todelete
   mv * todelete/
   mv todelete/world .
